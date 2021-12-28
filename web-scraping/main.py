f42e48f2208aa94477509ad116d7c89a08537e50
Burak Özkan <burak.ozkan@path.com.tr>
2021-12-28 16:23:26 +0300

    basic web scraping

---
+++ web-scraping/main.py
@@ -0,0 +1,1 @@
+import requestsfrom bs4 import BeautifulSoup as bsgithub_user = input('Github kullanıcı ismi: ')url = 'https://github.com/' + github_userr = requests.get(url)soup = bs(r.content, 'html.parser')profile_image = soup.find('img', {'alt', 'Avatar'})['src']print(profile_image)
